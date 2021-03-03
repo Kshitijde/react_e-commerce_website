ee515b322ee43010675fa8b9cae1f0ab71a83ce8
Kshitij Deshpande <kshitij.deshpande7@gmail.com>
2021-03-03 16:59:01 +0530

    Implemented firebase utils,included ability to store authenicated user to firestore db

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -38,8 +38,8 @@ class SignIn extends React.Component{
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
-                    name="email" t
-                    ype="email" 
+                    name="email" 
+                    type="email" 
                     value={this.state.email} 
                     handleChange={this.handleChange} 
                     label="email"
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 //functional component coz states in respective login and signup
 const SignInAndSignUp=()=>(
     <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 
 )
