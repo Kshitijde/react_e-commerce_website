bbab23e2f840ebdf11772b2f3b42420ba846b54f
Kshitij Deshpande <kshitij.deshpande7@gmail.com>
2021-07-03 19:27:41 +0530

    fixed styling

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
